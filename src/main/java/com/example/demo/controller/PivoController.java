790916141f345ee275383175a03ec9ab361866e2
anna-eleeva <anna-eleeva@yandex-team.ru>
2023-06-15 19:11:04 +0300

    cors

--- src/main/java/com/example/demo/controller/PivoController.java
+++ src/main/java/com/example/demo/controller/PivoController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/")
 public interface PivoController extends REST {
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "https://icy-sand-0229d2703.3.azurestaticapps.net")
     @GetMapping("/name")
     public String getRandomName();
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    //@CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "https://icy-sand-0229d2703.3.azurestaticapps.net")
     @GetMapping("/user")
     UserView getUser(@RequestParam(name = "userId", required = false) String userId);
 }
