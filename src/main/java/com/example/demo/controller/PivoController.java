c4c8adf7044bf3526a0be5f6ecac6ada7a3e667e
AnnaE <anna_eleeva@yahoo.com>
2024-04-30 15:32:14 +0500

    delete cors

--- src/main/java/com/example/demo/controller/PivoController.java
+++ src/main/java/com/example/demo/controller/PivoController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin
+//@CrossOrigin
 @RequestMapping("/api/")
 public interface PivoController extends REST {
 
-    @CrossOrigin(origins = "https://lemon-cliff-0ca7b9903.5.azurestaticapps.net")
+    //@CrossOrigin(origins = "https://lemon-cliff-0ca7b9903.5.azurestaticapps.net")
     @GetMapping("/name")
     public String getRandomName();
 
     //@CrossOrigin(origins = "http://localhost:3000")
-    @CrossOrigin(origins = "https://lemon-cliff-0ca7b9903.5.azurestaticapps.net")
+    //@CrossOrigin(origins = "https://lemon-cliff-0ca7b9903.5.azurestaticapps.net")
     @GetMapping("/user")
     UserView getUser(@RequestParam(name = "userId", required = false) String userId);
 }
