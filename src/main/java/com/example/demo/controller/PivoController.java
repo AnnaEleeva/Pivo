c8cdcea15f66953c31d7ec72dbb9b24b840c357c
AnnaE <anna_eleeva@yahoo.com>
2024-04-30 13:29:32 +0500

    fixme

--- src/main/java/com/example/demo/controller/PivoController.java
+++ src/main/java/com/example/demo/controller/PivoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin
 @RequestMapping("/api/")
 public interface PivoController extends REST {
+    //fixme
 
     @CrossOrigin(origins = "https://icy-sand-0229d2703.3.azurestaticapps.net")
     @GetMapping("/name")
